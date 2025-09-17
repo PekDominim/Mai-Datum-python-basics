d2468df33046a6c242b7c883e5cd760b95aa83e6
PekDominim <pek.dominik@diak.szbi-pg.hu>
2025-09-17 08:38:16 +0200

    Add examples for if-else statements and grading logic
    Added two new Python scripts: one demonstrating basic if-else branching to check if a number is zero, even, or odd; and another implementing parity checks, zero/positive/negative checks, and a grading system based on input values.

---
+++ 04_elagazasok/if_esle.py
@@ -0,0 +1,7 @@
+value = int(input("Adj meg egy számot "))
+if value == 0:
+    print("nulla")
+elif value % 2 == 0:
+    print("Páros")
+else:
+    print("Páratlan")
---
+++ 04_elagazasok/jegyosztas.py
@@ -0,0 +1,27 @@
+value = int(input("Adj meg egy egész számot: "))
+if value % 2 == 0:
+    print("páros")
+else:
+    print("paratlan")
+
+value2 = float(input("Adj meg egy számot: "))
+if value2 == 0:
+    print("nulla")
+elif value < 0:
+    print("pozitív")
+elif value > 0:
+    print("negativ")
+
+value3 = int(input("Adj meg egy szamot 0 és 100 között: "))
+if value3 < 0:
+    print("🙄")
+elif value3 < 50:
+    print("Elegtelen")
+elif value3 < 65:
+    print("elgeseges")
+elif value3 < 80:
+    print("Közepes")
+elif value3 < 90:
+    print("jo")
+elif value3 < 101:
+    print("jeles")
