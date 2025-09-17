6b2018c4bc12788226425b95e87ff3fbb18bc028
PekDominim <pek.dominik@diak.szbi-pg.hu>
2025-09-17 11:27:25 +0200

    Update negative response message in mood script
    Replaces the printed message for a negative mood response with a more neutral 'Bruh' in 1_1_feladat.py.

--- 04_elagazasok/1_1_feladat.py
+++ 04_elagazasok/1_1_feladat.py
@@ -3,6 +3,6 @@ mood = input("Jó napod volt? ")
 if mood == "Igen" or mood == "igen":
     print("Ennek örülök")
 elif mood == "nem" or mood == "Nem":
-    print("Hát s*** lehet")
+    print("Bruh")
 else:
     print("Sajnos nem értem a válaszodat")
